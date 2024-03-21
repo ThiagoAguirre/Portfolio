1418b0b82bd1ac3aa07cfb1b3083914522bd0bc7
ThiagoAguirre <thiago.aguirre08@outlook.com>
2024-03-21 17:30:28 -0300

    New test

--- Send.php
+++ Send.php
@@ -1,20 +1,16 @@
-<?php
-
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $telefone = addslashes($_POST['telefone']);
-
-    $para = "thiagoaguirre.profissional@gmail.com";
-    $assunto = "ASSUNTO IMPORTANTE";
-
-    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone;
-
-    $cabeca = "From: thiago.aguirre08@outlook.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
-
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o email!");
-    }
-
+<?php 
+$nome = $_POST['nome'];
+$email= $_POST['email'];
+$assunto= $_POST['assunto'];
+$mensagem= $_POST['mensagem'];
+$to = "thiagoaguirre.profissional@gmail.com";
+$subject = "Email Repositorio";
+$txt ="Name = ". $nome . "\r\n  Email = " . $email . "\r\n Message =" . $mensagem;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
 ?>
